3b3c6c318bde3a227309bd7a9e1be149d6a768f3
simplon-davidt <dtarcz@gmail.com>
2015-11-19 15:23:46 +0100

    amélioration correction avec le bonus 2

--- javascript/voyelles-correction.js
+++ javascript/voyelles-correction.js
@@ -1,23 +1,26 @@
-var strSaisie = prompt("Saisir une phrase");
-
 var tabVoyelles = ["a", "e", "i", "o", "u", "y"];
-var intCptVoyelles = 0;
 
-for (var i = 0; i < strSaisie.length; i++) {
-  var strLettre = strSaisie.substring(i, i+1);
+do {
+  var strSaisie = prompt("Saisir une phrase");
+  var intCptVoyelles = 0;
 
-  if (tabVoyelles.indexOf(strLettre) != -1) {
-    console.log(strLettre + " est une voyelle.");
-    intCptVoyelles++;
+  for (var i = 0; i < strSaisie.length; i++) {
+    var strLettre = strSaisie.substring(i, i+1);
+
+    if (tabVoyelles.indexOf(strLettre) != -1) {
+      console.log(strLettre + " est une voyelle.");
+      intCptVoyelles++;
+    }
   }
-}
 
-alert('Il y a ' + intCptVoyelles + ' Voyelles');
+  alert('Il y a ' + intCptVoyelles + ' Voyelles');
 
-if (intCptVoyelles <= 5) {
-  alert('Pas assez de voyelles');
-} else if (intCptVoyelles >= 6 && intCptVoyelles <= 10) {
-  alert("C'est presque bon...");
-} else if (intCptVoyelles > 10) {
-  alert("Nombre de voyelles OK !")
-}
+  if (intCptVoyelles <= 5) {
+    alert('Pas assez de voyelles');
+  } else if (intCptVoyelles >= 6 && intCptVoyelles <= 10) {
+    alert("C'est presque bon...");
+  } else if (intCptVoyelles > 10) {
+    alert("Nombre de voyelles OK !")
+  }
+
+} while (intCptVoyelles <= 10);
